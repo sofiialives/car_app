2ce5fbdfac53fa39f826cd5a01d04577b889791e
sofiialives <127687828+sofiialives@users.noreply.github.com>
2024-02-18 02:02:14 +0100

    refactoring

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,40 +6,30 @@ import { NavListStyled, NavStyled } from "./Header.styled";
 const Header = () => {
   const location = useLocation();
 
+  const routes = [
+    { route: "/", name: "Home" },
+    { route: "/catalog", name: "Catalog" },
+    { route: "/favorites", name: "Favorites" },
+  ];
+
   return (
     <NavStyled className="container">
       <NavLink to="/">
         <img src={logo} alt="logo" width={150} />
       </NavLink>
       <NavListStyled>
-        <li>
-          <NavLink
-            to="/"
-            className={`link ${location.pathname === "/" ? "active" : ""}`}
-          >
-            Home
-          </NavLink>
-        </li>
-        <li>
-          <NavLink
-            to="/catalog"
-            className={`link ${
-              location.pathname === "/catalog" ? "active" : ""
-            }`}
-          >
-            Catalog
-          </NavLink>
-        </li>
-        <li>
-          <NavLink
-            to="/favorites"
-            className={`link ${
-              location.pathname === "/favorites" ? "active" : ""
-            }`}
-          >
-            Favorites
-          </NavLink>
-        </li>
+        {routes.map((item, index) => (
+          <li key={index}>
+            <NavLink
+              to={item.route}
+              className={`link ${
+                location.pathname === `${item.route}` ? "active" : ""
+              }`}
+            >
+              {item.name}
+            </NavLink>
+          </li>
+        ))}
       </NavListStyled>
     </NavStyled>
   );
